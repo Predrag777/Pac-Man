fefe6554bc426a47ab2e4fe799f54fce49337d2c
Predrag <predragbrajovicSS@gmail.com>
2024-10-22 20:38:02 +0200

    wwww
    wwwwwwwww

--- main.py
+++ main.py
@@ -21,3 +21,18 @@ class Network(nn.Module):
         self.bn3 = nn.BatchNorm2d(64)
         self.conv4 = nn.Conv2d(64, 128, kernel_size=3, stride=1)
         self.bn4 = nn.BatchNorm2d(128)
+
+        self.fc1=nn.Linear(10*10*128, 512)
+        self.fc2=nn.Linear(512, 256)
+        self.fc2=nn.Linear(256, action_size)
+
+    def forward(self, state):
+       x=F.relu(self.bn1( self.conv1(state)))
+       x=F.relu(self.bn2( self.conv2(x)))
+       x=F.relu(self.bn3( self.conv3(x)))
+       x=F.relu(self.bn4( self.conv4(x)))
+       x=x.view(x.size(0), -1)
+       x=F.relu(self.fc1(x))
+       x=F.relu(self.fc2(x))
+       return x
+
