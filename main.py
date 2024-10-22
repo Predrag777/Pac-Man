e0a2e01c8ebf52a5eb37c123a298bc5b028eec72
Predrag <predragbrajovicSS@gmail.com>
2024-10-22 20:25:39 +0200

    jdjd
    cecimf

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import random
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.autograd as autograd
+from torch.autograd import Variable
+from collections import deque, namedtuple
+
+class Network(nn.Module):
+    def __init__(self, action_size, seed=42):
+        super(Network, self).__init__()
+        self.seed=torch.manual_seed(seed)
+        self.conv1=nn.Conv2d(3, 32, kernel_size=8, stride=4)
+        self.bn1=nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.conv4 = nn.Conv2d(64, 128, kernel_size=3, stride=1)
+        self.bn4 = nn.BatchNorm2d(128)
